15a9783c65d100b826ff2b5cedc443e7808714ec
damaera <damaera@ruangguru.com>
2020-06-01 21:08:47 +0700

    clean generated

--- components/Responsive.gen.tsx
+++
@@ -1,8 +0,0 @@
-/* TypeScript file generated from Responsive.re by genType. */
-/* eslint-disable import/first */
-
-
-// tslint:disable-next-line:no-var-requires
-const ResponsiveBS = require('./Responsive.bs');
-
-export const useWindowWidth: () => number = ResponsiveBS.useWindowWidth;
